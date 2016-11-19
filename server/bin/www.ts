7eb05afd37147a638ae73f211feefdfe07888070
Leek <dengl11@stanford.edu>
2016-11-19 00:01:53 -0800

    ready to implement simulation in server

--- server/bin/www.ts
+++ server/bin/www.ts
@@ -1,19 +1,25 @@
 #!/usr/bin/env node
+
 var app = require('../server').app;
 var debug = require('debug')('server:server');
 var http = require('http');
+
 var assert = require('assert');
+
 var port = '3000';
 app.set('port', port);
+
 var server = http.createServer(app);
+
 server.listen('3000');
 server.on('listening', onListening);
+
 function onListening() {
-    var addr = server.address();
-    var bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-    debug('Listening on ' + bind);
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
 }
 //
 // app.get('/compute/', function (request, response) {
@@ -22,4 +28,6 @@ function onListening() {
 //    console.log(a);
 //    response.end();
 // });
-//# sourceMappingURL=www.js.map
+
+
+
