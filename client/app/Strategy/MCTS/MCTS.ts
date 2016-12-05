0960ea60c1eac1c617470c74609de49cc15bc1bc
dengl11 <dengl11@stanford.edu>
2016-12-04 21:24:01 -0800

    no learning mongo

--- client/app/Strategy/MCTS/MCTS.ts
+++ client/app/Strategy/MCTS/MCTS.ts
@@ -2,11 +2,18 @@
 import { Agent } from '../Agent/Agent'
 import { State } from '../State/State'
 import { TDLeaner } from '../TDLearner/TDLearner'
+import { MoveReorderPruner } from '../MoveReorderPruner/MoveReorderPruner'
 
 export class MCTS extends TDLeaner {
+
+    weights = [20, 15, 30, 7, 20, 0, 20];
 
     strategy = 5;
     copy() {
         return new MCTS(this.team, this.DEPTH, this.weights, this.myPieces.map(x => x.copy()), this.copyMoves());
+    }
+
+    update_weights() {
+        return this.weights;
     }
 }
