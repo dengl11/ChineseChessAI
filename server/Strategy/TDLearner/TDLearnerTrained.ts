863766967ffba9b48ba805b05ad511a725c0ee6e
Leek <dengl11@stanford.edu>
2016-12-05 01:20:11 -0800

    for poster git add .

--- server/Strategy/TDLearner/TDLearnerTrained.ts
+++ server/Strategy/TDLearner/TDLearnerTrained.ts
@@ -7,13 +7,13 @@ import { Evaluation } from '../_Param/Evaluation'
 import { TDLearner } from './TDLearner'
 import { Reorder } from '../Reorder/Reorder'
 
-export class TDLearnerTrained extends Reorder {
+export class TDLearnerTrained extends TDLearner {
     strategy = 4;
     weights = [20, 15, 30, 7, 20, 0, 20];
 
 
     static copyFromDict(dict) {
-        return new TDLearnerTrained(dict.team, this.piecesFromDict(dict.myPieces), dict.DEPTH);
+        return new TDLearnerTrained(dict.team, this.piecesFromDict(dict.myPieces), dict.DEPTH, this.weights);
     }
 
 
